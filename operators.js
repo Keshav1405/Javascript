4ddfe6fcb6386ddb5b7e36b6b2461b1c4916147c
Keshav1405 <109613148+Keshav1405@users.noreply.github.com>
2022-08-25 19:36:27 +0530

    Update operators.js

--- operators.js
+++ operators.js
@@ -12,6 +12,29 @@ const Is_Valid = (a,b) => {
     return (a<10) && (a>b)
   };
 
+// Q3. You are given two numbers A and B. You need to do the following checks:
+// 1. if both are divisible by 10 console true.
+// 2. if both are not divisible by 10 console false.
+// 3. if one of them only is divisible by 10 console true.
+// Use operator to do this.
+// Note: You have to complete Check function. No need to take any input.
+
+const Check = (A, B) => 
+{
+  if ((A%10==0) && (B%10==0))
+  {
+    return true;
+  }
+  else if ((A%10!=0) && (B%10!=0))
+  {
+    return false;
+  }
+  else 
+  {
+    return true;
+  }
+};
+
 // Q4. You are provided a four digit number N only. Your task is to print out the first digit of that number.
 // Note: You have to complete First_Digit function. No need to take any input.
 
